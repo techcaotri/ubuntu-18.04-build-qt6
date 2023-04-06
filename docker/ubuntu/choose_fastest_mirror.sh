90f63980854ae5242563fc398ad2854cafd5d254
Tri Pham <techcaotri@gmail.com>
2023-04-06 15:03:33 +0700

    Choose fastest mirror using the 'mirror' method of apt-get

--- docker/ubuntu/choose_fastest_mirror.sh
+++ docker/ubuntu/choose_fastest_mirror.sh
@@ -1,24 +1,10 @@
 #!/bin/bash -x
 
-# Install netselect if not installed
-if ! command -v netselect &> /dev/null; then
-  wget http://ftp.us.debian.org/debian/pool/main/n/netselect/netselect_0.3.ds1-29_amd64.deb
-  dpkg -i netselect_0.3.ds1-29_amd64.deb
-fi
-
-# Get the list of mirrors
-mirror_list_url="http://mirrors.ubuntu.com/mirrors.txt"
-mirrors=$(wget -qO- "${mirror_list_url}")
-
-# Find the fastest mirror using netselect
-fastest_mirror=$(netselect -v -s 1 -t 20 ${mirrors} | grep -o -P "(f|ht)tp://[^\"]*")
-echo "${fastest_mirror}"
-
 # Backup the original sources.list
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 # Replace the current mirror with the fastest mirror
-sed -i "s|http://[^ ]\+|${fastest_mirror}|g" /etc/apt/sources.list
+sed -i "s|http://[^ ]\+|mirror://mirrors.ubuntu.com/mirrors.txt|g" /etc/apt/sources.list
 
 # Update the package list
 apt-get update
