5ab96d45e4439808b1154b911e6085bee87c1d91
Tri Pham <techcaotri@gmail.com>
2023-04-07 18:07:16 +0700

    Only change the original mirrors in sources.list and not touching the new added mirrors, e.g. llvm, etc.

--- docker/ubuntu/choose_fastest_mirror.sh
+++ docker/ubuntu/choose_fastest_mirror.sh
@@ -4,7 +4,9 @@
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 # Replace the current mirror with the fastest mirror
-sed -i "s|http://[^ ]\+|mirror://mirrors.ubuntu.com/mirrors.txt|g" /etc/apt/sources.list
+# sed -i "s|http://[^ ]\+|mirror://mirrors.ubuntu.com/mirrors.txt|g" /etc/apt/sources.list
+sed -i -E "s/http:\/\/(archive|security)[^ ]+/mirror:\/\/mirrors.ubuntu.com\/mirrors.txt/g" /etc/apt/sources.list
+cat /etc/apt/sources.list
 
 # Update the package list
 apt-get update
